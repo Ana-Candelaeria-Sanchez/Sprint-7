3b7773d1f1ae557f53946d82c283938d162fef75
Ana Sanchez <asnchez@frba.utn.edu.ar>
2022-08-21 16:46:44 -0300

    Problematica 1 terminada y comienzo de la 2

--- homebanking/clientes/views.py
+++ homebanking/clientes/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+
 # Create your views here.
+
+@login_required()
+def homebanking(request):
+    return render(request, "clientes/home.html", {})
---
+++ homebanking/login/forms.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.forms import TextInput, EmailInput, CharField, PasswordInput, NumberInput, DecimalField
+
+
+class CreateUserForm(UserCreationForm):
+    password1 = CharField(
+        label="Contraseña",
+        strip=False,
+        widget=PasswordInput(attrs={"autocomplete": "new-password", "class": "form-control mb-2", }),
+    )
+    password2 = CharField(
+        label="Confirmar contraseña",
+        widget=PasswordInput(attrs={"autocomplete": "new-password", "class": "form-control mb-3", }),
+        strip=False,
+        help_text="Ingrese la misma contraseña que arriba.",
+    )
+    dni = DecimalField(
+        label="DNI",
+        widget=NumberInput(attrs={"class": "form-control mb-2",
+                                  "placeholder": "DNI sin puntos", }),
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'dni', 'email', 'password1', 'password2', ]
+        widgets = {
+            'username': TextInput(attrs={
+                'class': 'form-control mb-2',
+                'placeholder': 'Nombre de usuario',
+            }),
+            'email': EmailInput(attrs={'class': 'form-control mb-2',
+                                       'placeholder': 'tu@mail.com',
+                                       }),
+            'first_name': TextInput(attrs={'class': 'form-control mb-2',
+                                           'placeholder': 'Nombre',
+                                           }),
+            'last_name': TextInput(attrs={'class': 'form-control mb-2',
+                                          'placeholder': 'Apellido',
+                                          }),
+        }
